e076267a0482ca2e0bf026a1c2b50a8b1343b92f
takeshi <jagabataa0141@gmail.com>
2016-07-11 17:30:04 +0900

    キーマトリクス完成

--- Matrix_test/Matrix_test.ino
+++ Matrix_test/Matrix_test.ino
@@ -4,33 +4,35 @@ void setup() {
   Serial.begin(115200);
   pinMode(50, OUTPUT);
   pinMode(52, OUTPUT);
-  pinMode(48, INPUT);
-  pinMode(46, INPUT);
-  digitalWrite(50, LOW);
-  digitalWrite(52, LOW);
+  digitalWrite(50, HIGH);
+  digitalWrite(52, HIGH);
   Serial.println("start");
 }
 
 void loop() {
   Get_TWEData data;
-  unsigned char btn;
-  bool btn1, btn2, btn3, btn4;
+  char allbtn;
+  int btn1, btn2, btn3, btn4;
+  int cnt = 0;
   while (1)
   {
+    Serial.println(cnt);
+    cnt++;
     digitalWrite(50, LOW);
-    delay(1000);
-    btn = data.getdi();
-    Serial.print("50pinH : "); Serial.println(btn, BIN);
-    digitalWrite(50, LOW);
-    delay(1000);
-    btn = data.getdi();
-    Serial.print("50pinL : "); Serial.println(btn, BIN);
-    Serial.println();
+    allbtn = data.getdi();
+    btn1 = allbtn & 0x01;
+    btn2 = allbtn & 0x02;
+    digitalWrite(50, HIGH);
+    digitalWrite(52, LOW);
+    allbtn = data.getdi();
+    btn3 = allbtn & 0x01;
+    btn4 = allbtn & 0x02;
+    digitalWrite(52, HIGH);
 
-    //    Serial.print("btn1 : ");Serial.println(btn1);
-    //    Serial.print("btn2 : ");Serial.println(btn2);
-    //    Serial.print("btn3 : ");Serial.println(btn3);
-    //    Serial.print("btn4 : ");Serial.println(btn4);
-    //    Serial.println();
+    Serial.print("btn1 : ");Serial.println(btn1, BIN);
+    Serial.print("btn2 : ");Serial.println(btn2, BIN);
+    Serial.print("btn3 : ");Serial.println(btn3, BIN);
+    Serial.print("btn4 : ");Serial.println(btn4, BIN);
+    Serial.println();
   }
 }
